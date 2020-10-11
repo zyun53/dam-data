66364537162964b4e70873e7ddca75ed7d8c5a81
Isida Zyun'iti <zyun@moin.jp>
2020-10-11 21:28:55 +0900

    Fix

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # dam_data
 
-dam data.
+## ダム便覧
+
+ZenItiIndex `http://damnet.or.jp/cgi-bin/binranA/ZenItiIndex.cgi?zi=zen&sy=go`
+
+* zi
+  * zen 全項目
+  * iti 一覧
+* sy
+  * go 五十音
+  * ken 都道府県
+
+## 国土数値情報
+
+http://nlftp.mlit.go.jp/ksj/
+
+ファイル名  ファイル容量  年度  測地系  地域  ダウンロード
+W01-14_GML.zip  0.36MB  平成26年  世界測地系  全国  
--- damnet.py
+++ damnet.py
@@ -1,7 +1,37 @@
 import requests
+from bs4 import BeautifulSoup
+import re
 
-url = 'http://damnet.or.jp/cgi-bin/binranA/ZenItiIndex.cgi?zi=iti&sy=go'
 
-r = requests.get(url)
+def top():
+    target_url = 'http://damnet.or.jp/cgi-bin/binranA/ZenItiIndex.cgi?zi=zen&sy=go'
+    r = requests.get(target_url)
+    r.encoding = r.apparent_encoding
 
-print(r)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    with open('originDataOld.html', mode='w', encoding = 'utf-8') as fw:
+        fw.write(soup.prettify())
+
+    elems = soup.find_all(href=re.compile("TableAllItiran.cgi.*kana"))
+    for e in elems:
+        get_all(e.get("href"))
+
+
+def get_all(url):
+    r = requests.get(url)
+    r.encoding = r.apparent_encoding
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    elems = soup.tr.tr.find_all('tr')
+    for e in elems:
+        text = e.find_all('b')
+
+        dam_name = text[0].getText().strip()
+        dam_address = text[1].getText()
+        dam_url = e.find('a').get('href')
+
+        print(dam_name + "," + dam_address + "," + dam_url)
+
+top()
